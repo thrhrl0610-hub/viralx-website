516e282aec36606cf43df37cc0330d423dbad601
박민규 <park@bagmingyuui-MacBookAir.local>
2026-04-06 21:48:15 +1200

    restore layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,9 +34,9 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body>{children}</body>
+      <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
 }
